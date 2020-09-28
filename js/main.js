42e52c43a33d87459ffb36f382a6736e611a6358
leoJ07 <joel@achievers.se>
2020-09-28 11:45:51 +0000

    loading animation started

--- js/main.js
+++ js/main.js
@@ -4,14 +4,20 @@ console.log(loadScrean, window.innerWidth)
 loadScrean.style.height = window.innerHeight + "px";
 loadingAnimation()
 
+animation = {
+  "loading": [0, 10, 20, 30, 40, 50, 60, 70, 80, 90],
+};
+
 function loadingAnimation(){
+  i++;
+  if(i >= Animation.loading.length) i = 0;
   if(loading){
     requestAnimationFrame(loadingAnimation);
   } else {
     console.log("done");
     return;
   }
-  console.log("loading");
+  console.log("loading", i);
 }
 
 import * as Inventory from "./inventory.js";
