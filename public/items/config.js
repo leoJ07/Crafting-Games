3d3f9923e448fdb72e00d6c7a78faa3f2ad763a2
leoJ07 <69954953+leoJ07@users.noreply.github.com>
2020-10-07 15:26:26 +0200

    Update config.js

--- public/items/config.js
+++ public/items/config.js
@@ -53,6 +53,13 @@ export var items = [
                 "outputPlace": 5,
                 "count": 4,
             },
+            {
+                "items": ["", "loog", "", ""],
+                "interfase": "2x2_crafting",
+                "shaped": true,
+                "outputPlace": 5,
+                "count": 4,
+            },
         ],
     },
 ];
