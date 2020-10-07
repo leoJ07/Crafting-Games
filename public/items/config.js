33273e54bb9022dcadff142ac2135d838c8a7916
leoJ07 <69954953+leoJ07@users.noreply.github.com>
2020-10-07 12:20:30 +0200

    Update config.js

--- public/items/config.js
+++ public/items/config.js
@@ -55,35 +55,4 @@ export var items = [
             },
         ],
     },
-    {
-        "name": "coding",
-        "texture": {
-            "item": {
-                "path": "Ultra-Codning.png",
-            },
-            "block": {
-                "path": {
-                    "upp": "",
-                    "down": "",
-                    "front": "",
-                    "back": "",
-                    "right": "",
-                    "left": "",
-                },
-            },
-        },
-        "data": {
-            "stack": 64,
-            "plasable": false,
-        },
-        "recipes": [
-            {
-                "items": ["loog", "loog", "loog", "loog"],
-                "interfase": "2x2_crafting",
-                "shaped": true,
-                "outputPlace": 5,
-                "count": 1,
-            },
-        ],
-    },
 ];
