89c74095fef9100807ac3aeb0667288988495940
leoJ07 <69954953+leoJ07@users.noreply.github.com>
2020-10-08 20:28:56 +0200

    Update config.js

--- public/items/config.js
+++ public/items/config.js
@@ -53,6 +53,72 @@ export var items = [
                 "outputPlace": 5,
                 "count": 4,
             },
+            {
+                "items": ["", "loog", "", ""],
+                "interfase": "2x2_crafting",
+                "shaped": true,
+                "outputPlace": 5,
+                "count": 4,
+            },
+            {
+                "items": ["", "", "loog", ""],
+                "interfase": "2x2_crafting",
+                "shaped": true,
+                "outputPlace": 5,
+                "count": 4,
+            },
+            {
+                "items": ["", "", "", "loog"],
+                "interfase": "2x2_crafting",
+                "shaped": true,
+                "outputPlace": 5,
+                "count": 4,
+            },
+        ],
+    },
+    {
+        "name": "coding",
+        "texture": {
+            "item": {
+                "path": "Ultra-Codning.png",
+            },
+            "block": {
+                "path": {
+                    "upp": "",
+                    "down": "",
+                    "front": "",
+                    "back": "",
+                    "right": "",
+                    "left": "",
+                },
+            },
+        },
+        "data": {
+            "stack": 64,
+            "plasable": true,
+        },
+        "recipes": [
+            {
+                "items": ["plank", "", "", "plank"],
+                "interfase": "2x2_crafting",
+                "shaped": true,
+                "outputPlace": 5,
+                "count": 2,
+            },
+            {
+                "items": ["", "plank", "plank", ""],
+                "interfase": "2x2_crafting",
+                "shaped": true,
+                "outputPlace": 5,
+                "count": 2,
+            },
+            {
+                "items": ["plank", "plank", "plank", "plank"],
+                "interfase": "2x2_crafting",
+                "shaped": true,
+                "outputPlace": 5,
+                "count": 4,
+            },
         ],
     },
 ];
