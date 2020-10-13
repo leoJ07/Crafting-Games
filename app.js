cbc571015894ab6c96373819cbc0559ab884be27
leoJ07 <69954953+leoJ07@users.noreply.github.com>
2020-10-13 20:25:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const app = express();
 
 app.use(express.static(__dirname + "/public/"));
 
-app.get("/", (req, res) => {
+app.get("", (req, res) => {
+    res.redirect("menu");
+})
+
+app.get("/menu", (req, res) => {
+    fs.readFile("menu.html", (err, data) => {
+        if(err){
+           console.log(err);
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+app.get("/game", (req, res) => {
     fs.readFile("index.html", (err, data) => {
         if(err) {
             console.log(err)
