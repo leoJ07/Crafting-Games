6f1b022e7e0d91d789f1130a1c290aab529e9737
leoJ07 <69954953+leoJ07@users.noreply.github.com>
2020-10-14 16:40:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ app.get("/", (req, res) => {
   res.redirect("/menu")
 })
 
-app.get("/game", (req, res) => {
+app.get("/play", (req, res) => {
     res.sendFile("./public/game.html", {root: __dirname});
+});
+
+app.get("/setings", (req, res) => {
+    res.sendFile("./public/setings.html", {root: __dirname});
+});
+
+app.get("/how_to_play", (req, res) => {
+    res.sendFile("./public/how_to_play.html", {root: __dirname});
 });
 
 app.get("/menu", (req, res) => {
