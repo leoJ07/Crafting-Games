db7ef96aa38e18a6dd45563dbff9c62226b71c14
leoJ07 <69954953+leoJ07@users.noreply.github.com>
2020-10-27 10:15:51 +0100

    Create recipes_config.js

---
+++ public/items/recipes_config.js
@@ -0,0 +1,49 @@
+/*
+{
+  "shaped": "",
+  "items": {
+    "0": "loog",
+    "1": "plank",
+    "2": "loog",
+    "3": "plank",
+  },
+  "interfase": "",
+  "result": {
+    "plase": 0,
+    "type": "",
+    "res": ""/() => {
+      
+    };
+  },
+}
+*/
+
+export var recipes = [
+  {
+    "shaped": false,
+    "items": ["loog", "", "", ""],
+    "interfase": "2x2_crafting",
+    "result": {
+      "plase": 4,
+      "type": "item",
+      "res": "plank",
+      "count": 4,
+    },
+  },
+  {
+    "shaped": true,
+    "items": [
+      "loog",
+      "plank",
+      "loog",
+      "plank",
+    ],
+    "interfase": "2x2_crafting",
+    "result": {
+      "plase": 4,
+      "type": "item",
+      "res": "coding",
+      "count": 1,
+    },
+  },
+];
